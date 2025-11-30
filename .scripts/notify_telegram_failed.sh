f46ff7b760a4b9b1ab640c83a72ae5148d66810b
Dimas Prabowo <58059518+98prabowo@users.noreply.github.com>
2025-11-30 16:03:11 +0700

    chore: refactor telegram notification into dedicated script files (#3)
    * chore: refactor telegram notification into different script files
    
    * fix: update env variable from github
    
    * chore: fix typo in string literal

---
+++ .scripts/notify_telegram_failed.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+TEXT=$(
+  cat <<EOF
+❌ *Smart Contract Test Failed!*
+
+Repository: [${GITHUB_REPOSITORY}](${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY})
+Branch: [${GITHUB_REF_NAME}](${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/tree/${GITHUB_REF_NAME})
+By: [${GITHUB_ACTOR}](${GITHUB_SERVER_URL}/${GITHUB_ACTOR})
+
+*Commits:*
+${COMMITS}
+
+🧩 [View Workflow Logs](${GITHUB_SERVER_URL}/${GITHUB_REPOSITORY}/actions/runs/${GITHUB_RUN_ID})
+EOF
+)
+TEXT_URLENCODED=$(echo "$TEXT" | jq -s -R -r @uri)
+
+curl -s -X POST "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendMessage" \
+  -d chat_id="${TELEGRAM_CHAT_ID}" \
+  -d message_thread_id="${TELEGRAM_TOPIC_ID}" \
+  -d text="$TEXT_URLENCODED" \
+  -d parse_mode="Markdown" \
+  -d disable_web_page_preview="true"
